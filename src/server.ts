5f888128a3fe1e563cee886c7a16f4ad6a68911f
rmar72 <rsolm72@gmail.com>
2025-01-25 15:46:42 -0700

    cfont addition for cool server start up log

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,52 @@
 import app from './app';
+import CFonts from 'cfonts';
+import logger from './utils/logger';
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+const server = app.listen(PORT, () => {
+  CFonts.say('Nooro API', {
+    font: 'block',
+    align: 'center',
+    colors: ['cyan', 'yellow'],
+    background: 'transparent',
+    letterSpacing: 0.1,
+    lineHeight: 0,
+    space: false,
+  });
+  CFonts.say('Running on:', {
+    font: 'simple',
+    align: 'center',
+    colors: ['yellowBright'],
+    background: 'transparent',
+    letterSpacing: 0.3,
+    lineHeight: 0.4,
+    space: false,
+  });
+  CFonts.say(`http://localhost:${PORT}`, {
+    font: 'simple',
+    align: 'center',
+    colors: ['cyan', 'blue'],
+    background: 'transparent',
+    letterSpacing: 0,
+    lineHeight: 0.3,
+    space: false,
+  });
+});
+
+// Gracefully handle shutdown signals
+function shutdown() {
+  logger.warn('The server is shutting down...');
+  server.close((err) => {
+    if (err) {
+      logger.error(`Error during shutdown: ${err}`);
+      process.exitCode = 1;
+    }
+    process.exit();
+  });
+}
+
+// Attach shutdown logic to signals
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
+
